43b50f1b8d5eebb6ecf23977eaaf95b523bd7c20
Светлана Щеглова <svetlanaseglova1@gmail.com>
2021-01-05 12:44:26 +0300

    index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunction() {
-	var x = document.getElementsById("myheader__menu")
-
-	if(x.className === "header__menu") {
-		x.className += "responsive";
-	} else{
-		x.className = "header__menu"
-	}
-}
